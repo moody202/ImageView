9d2e937ac602af998cda801393b3bbc6d92e8249
moody202 <mahmoudabdelhakam89@gmail.com>
2022-01-20 09:22:21 +0300

    image
    image

---
+++ MoveActivity/app/src/main/java/com/ayoubschool/moveactivity/oneActivity.java
@@ -0,0 +1,15 @@
+package com.ayoubschool.moveactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class oneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
